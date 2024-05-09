f981d90d21dc74c67467f2a79bdfea7c886eff33
Hossain <arafat.kd99@gmail.com>
2024-05-09 22:46:53 +0100

    Configure routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './styles/App.css';
 
-const App = () => (
-  <div className="bg-gray-100 p-4">
-    <h1 className="text-3xl font-bold text-center">Hello, Tailwind CSS!</h1>
-    <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" type="button">
-      Button
-    </button>
-  </div>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
